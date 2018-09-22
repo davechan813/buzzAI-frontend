710cbef93b95ba1938f816f18e007b9135a8f239
haejinjo <professionalhaejin@gmail.com>
2018-09-22 02:10:46 -0700

    First commit. Dummy USA query for buzzwords. Displays all 50 results, sorted by hit count in descending order, in table in page. TODO: 1-5 pagination with material-ui, refactoring to make more modular code.

---
+++ src/components/BuzzWordData.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import axios from 'axios';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import TablePagination from '@material-ui/core/TablePagination';
+import Typography from '@material-ui/core/Typography';
+import { isEqual, isEmpty, differenceWith } from 'lodash';
+
+const styles = theme => ({
+  root: {
+    // justifyContent: 'center',
+    margin: theme.spacing.unit * 3,
+    flexWrap: 'wrap',
+    textAlign: 'center',
+    overflowX: 'auto',
+    // alignContent: 'center'
+  },
+
+  table: {
+    minWidth: 700,
+  },
+
+  title: {
+    flex: '0 0 auto',
+  }
+});
+
+let isArrayEqual = (x, y) => {
+  return isEmpty(differenceWith(x, y, isEqual));
+}
+
+let popularityCompare = (x, y) => {
+  let keyA = x.popularity_count,
+    keyB = y.popularity_count;
+
+  return keyB - keyA;
+}
+
+let id = 0;
+
+class BuzzWordData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+
+  addBuzzWordToState = (arrayData) => {
+
+    let word = arrayData.name;
+    let url = arrayData.url;
+    let count = arrayData.tweet_volume;
+
+    let obj = {
+      id: id, //defined above class header
+      buzz_word: word,
+      source_url: url,
+      popularity_count: count,
+    }
+
+    id += 1;
+
+    this.setState({ data: [...this.state.data, obj] });
+  }
+
+  handleSearch = () => {
+    let self = this;
+
+    axios.post('http://buzzai-env-2.us-east-2.elasticbeanstalk.com/buzz10', {
+      placeName: 'United States', // dummy location search query for now 
+    })
+      .then(function (response) {
+        console.log("the response is:", self.state.data);
+
+        let trendsArray = response.data[0].trends;
+        trendsArray.sort(popularityCompare);
+        let dataHasChanged = isArrayEqual(self.state.data, trendsArray);
+
+        if (dataHasChanged) {
+          for (var i = 0; i < trendsArray.length; i++) {
+            self.addBuzzWordToState(trendsArray[i]);
+          }
+        }
+
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { classes } = this.props;
+    let rows = this.state.data.slice();
+    rows.sort(popularityCompare);
+
+    return (
+
+      <Paper className={classes.root}>
+        <div className={classes.title}>
+          <Typography variant="title" id="tableTitle">
+            Today's Buzzwords
+        </Typography>
+        </div>
+        <Button onClick={this.handleSearch}>Search in USA</Button>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Buzzword</TableCell>
+              <TableCell>Hit Count</TableCell>
+              <TableCell>Source Link</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows.map(row => {
+              return (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.buzz_word}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.popularity_count}
+                  </TableCell>
+                  <TableCell component="th" scope="row">
+                    {row.source_url}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
+
+BuzzWordData.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BuzzWordData);
--- src/pages/BuzzWord.js
+++ src/pages/BuzzWord.js
@@ -3,6 +3,9 @@ import NavBar from '../components/NavBar';
 import InputGroup from '../components/InputGroup';
 import DataDisplay from '../components/DataDisplay';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import { Button, Switch } from '@material-ui/core';
+import BuzzWordData from '../components/BuzzWordData';
 
 class BuzzWord extends Component {
 	constructor(props) {
@@ -12,15 +15,15 @@ class BuzzWord extends Component {
 		};
 	}
 
-	setData = (data) => this.setState({data: data});
+	setData = (data) => this.setState({ data: data });
 
 	render() {
 		return (
 			<div>
-				<Typography variant='display2'>Building</Typography>
+				<BuzzWordData />
 			</div>
 		);
 	}
 }
-  
+
 export default BuzzWord;
